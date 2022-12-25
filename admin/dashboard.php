8b047df99b5fd3fa962ecaf68cbf60390f2e81f2
aimedidierm <aimedidiermugisha@gmail.com>
2022-12-25 11:22:21 +0200

    Sub subscription

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -20,10 +20,6 @@ $stmt = $db->prepare($sql);
 $stmt->execute();
 $users=$stmt->rowCount();
 
-$sql = "SELECT * FROM sub";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-$subs=$stmt->rowCount();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -96,29 +92,6 @@ $subs=$stmt->rowCount();
                         </div>
                     </div>
                 </div>
-
-                <!-- Earnings (Monthly) Card Example -->
-                <div class="col-xl-3 col-md-6 mb-4">
-                    <div class="card border-left-info shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Subscription
-                                    </div>
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col-auto">
-                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $subs;?></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
                 <!-- Pending Requests Card Example -->
                 <div class="col-xl-3 col-md-6 mb-4">
                     <div class="card border-left-warning shadow h-100 py-2">
